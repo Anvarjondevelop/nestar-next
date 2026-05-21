eebdb9967f1fea1c47f0ca9e4a92cd47f96c6ff6
Anvarjon <anvarjondevelop@gmail.com>
2026-05-21 20:10:14 +0900

    feat: develop MyFavorites and RecentlyVisited components logic

--- libs/components/mypage/MyFavorites.tsx
+++ libs/components/mypage/MyFavorites.tsx
@@ -5,6 +5,12 @@ import { Pagination, Stack, Typography } from '@mui/material';
 import PropertyCard from '../property/PropertyCard';
 import { Property } from '../../types/property/property';
 import { T } from '../../types/common';
+import { useMutation, useQuery, useReactiveVar } from '@apollo/client';
+import { userVar } from '../../../apollo/store';
+import { GET_FAVORITES } from '../../../apollo/user/query';
+import { LIKE_TARGET_PROPERTY } from '../../../apollo/user/mutation';
+import { Message } from '../../enums/common.enum';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../sweetAlert';
 
 const MyFavorites: NextPage = () => {
 	const device = useDeviceDetect();
@@ -13,10 +19,43 @@ const MyFavorites: NextPage = () => {
 	const [searchFavorites, setSearchFavorites] = useState<T>({ page: 1, limit: 6 });
 
 	/** APOLLO REQUESTS **/
+	const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
+
+	const {
+		loading: getFavoritesLoading,
+		data: getFavoritesData,
+		error: getFavoritesError,
+		refetch: getFavoritesRefetch,
+	} = useQuery(GET_FAVORITES, {
+		fetchPolicy: 'network-only',
+		variables: { input: searchFavorites },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setMyFavorites(data?.getFavorites?.list ?? []);
+			setTotal(data?.getFavorites?.metaCounter[0]?.total ?? 0);
+		},
+	});
 
 	/** HANDLERS **/
 	const paginationHandler = (e: T, value: number) => {
 		setSearchFavorites({ ...searchFavorites, page: value });
+	};
+
+	const likePropertyHandler = async (user: T, propertyId: string) => {
+		try {
+			if (!propertyId) return;
+			if (!user?._id) throw new Error(Message.NOT_AUTHENTICATED);
+
+			await likeTargetProperty({
+				variables: { input: propertyId },
+			});
+
+			await getFavoritesRefetch({ input: searchFavorites });
+			sweetTopSmallSuccessAlert('Successfully liked property', 800);
+		} catch (err: any) {
+			console.log('Error, likePropertyHandler:', err.message);
+			sweetMixinErrorAlert(err.message);
+		}
 	};
 
 	if (device === 'mobile') {
